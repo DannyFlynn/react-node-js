4def07e5825f68c4c8c5ca89460bce24771b90ac
Danny Flynn <flynny386@gmail.com>
2022-05-11 13:04:42 +0100

    hidden db password in .env

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,12 @@
 //creating connection to database
+require('dotenv').config();
+
 const mysql = require('mysql');
 
 var DB = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "12345",
+    password: process.env.PASSWORD,
     database: "users_database"
   });
 
